db064efe0f203fc66738a89d8963665970ce1d00
gabrielrm72660 <63435075+gabrielrm72660@users.noreply.github.com>
2025-12-30 13:33:23 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  // Isso avisa ao sistema para ativar o modo escuro quando encontrar a classe "dark" no HTML
-  darkMode: 'class', 
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      // Aqui garantimos que o indigo e o slate funcionem bem
-    },
+    extend: {},
   },
   plugins: [],
 }
